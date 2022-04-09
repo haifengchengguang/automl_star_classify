29d448bc7fe3c7b0ec7f448153713a38c94a5f70
haifengchengguang <60420171+haifengchengguang@users.noreply.github.com>
2022-04-09 20:20:32 +0800

    二分类

--- brownd.py
+++ brownd.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 data_path= "automl_train_data.csv"
 df=pd.read_csv(data_path) # path to colab notebook #replace this with the path to your dataset
 
-bd_data = TabularDataset(data_path)
+#bd_data = TabularDataset(data_path)
 df_train,df_test=train_test_split(df,test_size=0.2,random_state=1)
 #test_data = TabularDataset('https://autogluon.s3.amazonaws.com/datasets/Inc/test.csv')
 save_path = './BDmodels/'
